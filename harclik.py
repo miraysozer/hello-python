4141af7093cd8c940e352b3a35ad324bbc644520
miraysozer <miraysozeer@gmail.com>
2025-04-10 19:38:57 -0700

    add harclik.py

---
+++ harclik.py
@@ -0,0 +1,9 @@
+gunluk_harclik = input("gunluk harcligini gir : ")
+gun_sayisi = input("kac gun boyunca biriktireceksin? ")
+
+gunluk_harclik = float(gunluk_harclik)
+gun_sayisi = int(gun_sayisi)
+
+Toplam = gunluk_harclik * gun_sayisi
+
+print("toplam biriktirecegin miktat:", toplam)
